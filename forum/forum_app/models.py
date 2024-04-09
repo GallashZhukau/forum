ba7904f0ad55dcf9532cb6cc587f7c4f5d01abd4
name <zhukov.vladimerovich@gmail.com>
2024-04-09 20:35:27 +0300

    -clear db by older migrations

--- forum/forum_app/models.py
+++ forum/forum_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+from datetime import date
 from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 from django.contrib.auth.models import User #Blog author or commenter
 
